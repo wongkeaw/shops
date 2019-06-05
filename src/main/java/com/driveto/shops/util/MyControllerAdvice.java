5fe07c642f54d67f88d31108fc258f0ce45a27fd
wongkeaw <oa.wongkeaw@gmail.com>
2019-06-06 01:04:30 +0700

    init

---
+++ src/main/java/com/driveto/shops/util/MyControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.driveto.shops.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice()
+public class MyControllerAdvice {
+
+    @Value("${shops.app.version}")
+    private String applicationVersion;
+
+    @ModelAttribute("applicationVersion")
+    public String getApplicationVersion() {
+        return applicationVersion;
+    }
+
+}
